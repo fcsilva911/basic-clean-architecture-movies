2fa102b808ce24cad418c2b16fc067e40daf1256
fcsilva911 <fc.silva.911@gmail.com>
2023-09-16 22:41:27 +0100

    base projects

---
+++ CleanMovie/CleanMovie.API/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        // GET: api/movies
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var movies = _movieService.GetAllMovies();
+            return Ok(movies);
+        }
+
+    }
+}
