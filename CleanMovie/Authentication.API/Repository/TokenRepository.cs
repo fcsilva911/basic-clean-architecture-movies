8f1330e807ab50e66ac0a3035d351e8b126c99bb
fcsilva911 <fc.silva.911@gmail.com>
2023-09-27 23:57:32 +0100

    Add JWT authentication project

---
+++ CleanMovie/Authentication.API/Repository/TokenRepository.cs
@@ -0,0 +1,50 @@
+﻿using Authentication.API.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Authentication.API.Repository
+{
+    public class TokenRepository : ITokenRepository
+    {
+        Dictionary<string, string> UsersRecords = new Dictionary<string, string>
+        {
+            { "admin","admin"},
+            { "password","password"}
+        };
+
+        private readonly IConfiguration _configuration;
+
+        public TokenRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Tokens Authenticate(Users users)
+        {
+            if (!UsersRecords.Any(x => x.Key == users.Name && x.Value == users.Password))
+            {
+                return null;
+            }
+
+            //Generate JSON Web Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, users.Name)
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _configuration["JWT.Issuer"],
+                Audience = _configuration["JWT.Audience"]
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return new Tokens { Token = tokenHandler.WriteToken(token) };
+        }
+    }
+}
